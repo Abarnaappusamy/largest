49ce2891a0fb5a82f1277bf03b1a42e7864bb9bf
Abarnaappusamy <abarnaappusamy@gmail.com>
2016-07-08 15:34:25 +0530

    finish

---
+++ largest.c
@@ -0,0 +1,19 @@
+# largest
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter three numberws");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+{
+printf("a is greater");
+}
+else if(b>c)
+{
+printf("b is greater");
+}
+else
+{
+printf("c is greater");
+}
